93746c13d0a4c73d36fd7287ec0a615522c6b61a
Syntaf <gmercer015@gmail.com>
2020-01-20 15:32:40 -0800

    Use new rails logger config

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module Wagbag
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    logger = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
